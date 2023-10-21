53c01ff03d7393a4dfeb0f456b9cf73de15a799d
NguyenBinh <binh.nt2064@sinhvien.hoasen.edu.vn>
2023-10-21 10:41:28 +0700

    push 8

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-document.getElementById('next').onclick = function(){
-    const widthItem = document.querySelector('.item').offsetWidth;
-    document.getElementById('formList').scrollLeft += widthItem;
-}
-document.getElementById('prev').onclick = function(){
-    const widthItem = document.querySelector('.item').offsetWidth;
-    document.getElementById('formList').scrollLeft -= widthItem;
-}
+document.getElementById("next").onclick = function () {
+  const widthItem = document.querySelector(".item").offsetWidth;
+  document.getElementById("formList").scrollLeft += widthItem;
+};
+document.getElementById("prev").onclick = function () {
+  const widthItem = document.querySelector(".item").offsetWidth;
+  document.getElementById("formList").scrollLeft -= widthItem;
+};
+
+// scroll to the area when click header is clicked
+const overlay = document.querySelector("#overlay");
+
+document.querySelector("#header").addEventListener("click", function () {
+  overlay.classList.toggle("show");
+});
